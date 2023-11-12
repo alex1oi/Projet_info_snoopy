a1af65eec7b14074cac6648c5ef137e56f275755
Pierre_Martinez <94850000+CrazyDuckR6@users.noreply.github.com>
2023-11-12 19:53:29 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,58 +1,6 @@
-#include <Arduino.h>
-const int led1 = 2;
-const int led2 = 3;
-const int led3 = 4;
-const int led4 = 5;
-const int sensor = A1;
-int temp;
+#include <stdio.h>
 
-void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(sensor, INPUT);
+int main() {
+    printf("Hello, World!\n");
+    return 0;
 }
-
-void loop() {
-temp = analogRead(sensor);
-
-if (temp < 10) {
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-}
-else
-if (temp < 15) {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-
-}
-else if (temp < 20) {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-
-}
-else if (temp < 25) {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, LOW);
-
-}
-else {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, HIGH);
-
-}
-
-delay(100);
-}
-
